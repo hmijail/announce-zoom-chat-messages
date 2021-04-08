4f2d041cc9f2c6fb075c34c1c8aaccadb3fd1fc4
Horacio Mijail Anton Quiles <hmijail@gmail.com>
2021-04-08 11:20:41 +1000

    Works on Zoom 5.6.1

--- announce-zoom-chat-messages.js
+++ announce-zoom-chat-messages.js
@@ -7,12 +7,61 @@ SE.strictCommandScope = true
 SE.includeStandardAdditions = true
 
 past_length = undefined
+dormant = false
+
+//get_chat_length()
+
+function is_meeting_ongoing() {
+ return (SE.applicationProcesses.byName("zoom.us").windows.whose({ name: { _contains: 'Zoom Meeting' } }).length == 1)
+}
+
+function get_chat_length(){
+	// returns the number of lines in chat (including the name lines!)
+
+    // get content of detached chat window
+    zwc = SE.applicationProcesses.byName("zoom.us").windows.whose({ name: { _contains: 'Chat' } })
+	if (zwc.length == 1) {
+		// there is a chat window
+    	zwcs = zwc.splitterGroups.at(0)
+    	zwcss = zwcs.scrollAreas.at(0)
+		zwcsst = zwcss.tables.at(0)
+		zwcsstr = zwcsst.rows
+		return zwcsstr.length
+    }
+
+    // get content of attached chat window
+    zwc2 = SE.applicationProcesses.byName("zoom.us").windows.whose({ name: { _contains: 'Zoom Meeting' } })
+
+    sidepanel = zwc2.splitterGroups[0]
+	chat_embedded = sidepanel.splitterGroups[0]
+	if (chat_embedded()[0] == null) {
+		// the chat panel is not where it should
+		return null
+	}
+
+	text_rows = chat_embedded.scrollAreas[0].tables[0].rows
+	return text_rows.length
+
+}
+
 
 function idle(){
+	if (! is_meeting_ongoing()) {
+		if (! dormant) {
+			// let's warn and go dormant
+			past_length = undefined
+			dormant = true
+			currApp.beep(1)
+        	currApp.displayNotification("Waiting for any new meeting...",
+            	{withTitle:"No Zoom meeting detected"})
+		}
+		return 30
+	}
+	dormant = false
     try {
-        text = get_chat_text()
+        length = get_chat_length()
     } catch (error) {
-        SE.displayAlert("Unable to get the chat text", {
+        SE.displayAlert("Unable to get the chat length", {
             message: "Please check that the script is allowed in System Preferences " +
                 "- Security & Privacy - Privacy - Accessibility.\n\nYou might need to " +
                 "UNCHECK its checkbox and RE-CHECK it again.",
@@ -27,56 +76,32 @@ function idle(){
         ObjC.import('stdlib')
         $.exit(1)
     }
-    if (!! text) {
-        l=text.length
-        if (typeof past_length == 'undefined'){
-            currApp.displayNotification("Currently contains " + l + " chars",
-                {withTitle:"Tracking chat..."})
-        } else if (l != past_length) {
-            text_new = text.slice(past_length)
-            text_new_asciipos = text_new.search(/\w/)
-            text_new_ascii = text_new.slice(text_new_asciipos)
-            currApp.beep(3)
-            currApp.displayNotification(text_new_ascii,
-                {withTitle:"New text in chat"})
-        }
-        past_length = l
-
-    } else  {
-        currApp.beep(1)
+    if (length == null) {
+		currApp.beep(1)
         currApp.displayNotification("Please open it to detect changes",
             {withTitle:"The chat window seems to be closed!"})
         past_length = undefined
         return 5
+    } else  {
+        if (typeof past_length == 'undefined'){
+            currApp.displayNotification("Currently contains " + length + " lines",
+                {withTitle:"Tracking chat..."})
+        } else if (length != past_length) {
+            //content_new = content.slice(past_length)
+            //text_new_asciipos = text_new.search(/\w/)
+            //text_new_ascii = text_new.slice(text_new_asciipos)
+            currApp.beep(3)
+            currApp.displayNotification("",
+                {withTitle:"New text in chat"})
+        }
+        past_length = length
     }
 
     return 1
 }
 
+
 function quit(){
     currApp.displayNotification("",{withTitle:"Chat is no longer tracked"})
     return true
-}
-
-function get_chat_text(){
-    // get text from the detached chat window
-    zwc = SE.applicationProcesses.byName("zoom.us").windows.whose({ name: { _contains: 'chat' } })
-    zwcs = zwc.splitterGroups.at(0)
-    zwcss0 = zwcs.scrollAreas.at(0)
-    zwcss0t = zwcss0.textAreas.at(0)
-    text = zwcss0t.value()[0]
-
-    if (!!text) {
-        return text
-    }
-
-    // get text from the attached chat window
-    zwc2 = SE.applicationProcesses.byName("zoom.us").windows.whose({ name: { _contains: 'Zoom Meeting' } })
-    zwc2s = zwc2.splitterGroups.at(0)
-    zwc2ss = zwc2s.splitterGroups.at(0)
-    zwc2sss = zwc2ss.scrollAreas.at(0)
-    zwc2ssst = zwc2sss.textAreas.at(0)
-    text = zwc2ssst.value()[0]
-
-    return text
 }
