35c7eaae907afda0e47d246499cd9c56aadea0cd
Jack Leow <jleow@tubi.tv>
2023-08-11 11:49:38 -0700

    Specified CLI tool name and abstract.

--- Sources/Main.swift
+++ Sources/Main.swift
@@ -5,6 +5,11 @@ import LoggingFormatAndPipe
 
 @main
 struct Main: ParsableCommand {
+    static let configuration = CommandConfiguration(
+        commandName: "zoom-chat-event-publisher",
+        abstract: "Scrapes Zoom chat messages and publishes them to an HTTP endpoint."
+    )
+    
     @Option(name: .shortAndLong, help: "The URL to publish chat messages to.")
     var destinationURL: URLComponents
     
